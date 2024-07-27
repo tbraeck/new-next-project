7f430e12f8f71956cdbbc8671f23bb938378e6a8
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-07-27 18:16:50 -0500

    button styling abd some simple link changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['img.freepik.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
